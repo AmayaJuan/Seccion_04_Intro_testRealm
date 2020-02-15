0c66ce1dda427604ea364042cea33a3100be4934
AmayaJuan <jpva360@gmail.com>
2020-02-15 18:09:06 -0500

    MyAdapter

--- app/src/main/java/com/jpvaapi/seccion_04_intro_testrealm/adapters/MyAdapter.java
+++ app/src/main/java/com/jpvaapi/seccion_04_intro_testrealm/adapters/MyAdapter.java
@@ -1,10 +1,14 @@
 package com.jpvaapi.seccion_04_intro_testrealm.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.jpvaapi.seccion_04_intro_testrealm.R;
+import com.jpvaapi.seccion_04_intro_testrealm.models.Dog;
 import com.jpvaapi.seccion_04_intro_testrealm.models.Person;
 
 import java.util.List;
@@ -42,6 +46,33 @@ public class MyAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder vh:
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(layout, null);
+            vh = new ViewHolder();
+            vh.id = convertView.findViewById(R.id.textVIewId);
+            vh.name = convertView.findViewById(R.id.textViewName);
+            vh.dogs = convertView.findViewById(R.id.textViewDogs);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        Person p = list.get(position);
+
+        vh.id.setText(p.getId() + "");
+        vh.name.setText(p.getName());
+        String dogsInfo = "";
+        for (Dog dog : p.getDogs())
+            dogsInfo += dog.getId() + "-" + dog.getName() + "\n";
+
+        vh.dogs.setText(dogsInfo);
         return convertView;
+    }
+
+    public class ViewHolder {
+        TextView id;
+        TextView name;
+        TextView dogs;
     }
 }
